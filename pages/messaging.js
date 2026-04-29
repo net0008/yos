16e36fc1f104fa8aa4da9e7a7895ec076a05e69c
net0008 <herdogmus@hotmail.com>
2026-04-29 20:55:45 +0300

    Update messaging.js

--- pages/messaging.js
+++ pages/messaging.js
@@ -5,12 +5,6 @@ import { createClient } from '@supabase/supabase-js';
 import { createServerClient } from '@supabase/ssr';
 import { serialize } from 'cookie';
 
-// Supabase istemcisini başlatın (Sadece sunucu tarafında kullanılacak)
-const supabaseAdmin = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
-);
-
 export default function MessagingPage({ currentUser, allUsers }) {
     if (!currentUser) {
         return (
