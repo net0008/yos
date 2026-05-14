c5868f5c8cb298d6e48ac126d76f6a918cead130
net0008 <herdogmus@hotmail.com>
2026-05-14 13:34:06 +0300

    qq

--- components/Layout.js
+++ components/Layout.js
@@ -9,15 +9,15 @@ export default function Layout({ children, title = 'YOS Uygulaması' }) {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="min-h-screen bg-gray-100">
-                <header className="bg-white shadow">
-                    <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-                        <h1 className="text-3xl font-bold leading-tight text-gray-900">
+            <div className="page-bg">
+                <header className="top-navbar">
+                    <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+                        <h1 className="nav-brand">
                             {title}
                         </h1>
                     </div>
                 </header>
-                <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <main className="container-main">
                     {children}
                 </main>
             </div>
