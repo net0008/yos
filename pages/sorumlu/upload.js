f336c41c49bb6aa92202bec720e367b1fb46212d
net0008 <herdogmus@hotmail.com>
2026-05-14 15:53:59 +0300

    rr

---
+++ pages/sorumlu/upload.js
@@ -0,0 +1,11 @@
+// pages/sorumlu/upload.js
+import Layout from '../../components/Layout';
+import ReportUploadForm from '../../components/ReportUploadForm';
+
+export default function SorumluUploadPage() {
+    return (
+        <Layout title="Rapor Yükle">
+            <ReportUploadForm />
+        </Layout>
+    );
+}
